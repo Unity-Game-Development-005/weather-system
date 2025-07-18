39c8a6419b1e2c7a7e52c37ce1618c64f0b8cd38
Andrew W. F. Hatton <7912262+Wanottah@users.noreply.github.com>
2025-07-18 16:24:27 +0100

    Week 11
    Weekly Challenge

--- Assets/WeeklyChallenge 11/Scripts/WeatherSystem.cs
+++ Assets/WeeklyChallenge 11/Scripts/WeatherSystem.cs
@@ -10,6 +10,8 @@ public class WeatherSystem : MonoBehaviour
     public Material globalMaterial;
     public Light sunLight;
     public Material skyboxMaterial;
+    public GameObject snowFX;
+    public GameObject rainFX;
     public TMP_Text weatherText;
 
     [Header("Winter Assets")]
@@ -28,6 +30,15 @@ public class WeatherSystem : MonoBehaviour
     public ParticleSystem summerParticleSystem;
     public Volume summerVolume;
 
+
+    private const int AUTUMN = 0;
+    private const int SPRING = 1;
+    private const int SUMMER = 2;
+    private const int WINTER = 3;
+
+    
+
+
     private void Start()
     {
         Summer();
@@ -35,22 +46,81 @@ public class WeatherSystem : MonoBehaviour
 
     public void Winter()
     {
-      
+        weatherText.text = "WINTER";
+
+        ChangeSeason(WINTER);
     }
 
     public void Rain()
     {
-       
+        weatherText.text = "SPRING";
+
+        ChangeSeason(SPRING);
     }
 
     public void Autumn()
     {
-    
+        weatherText.text = "AUTUMN";
+
+        ChangeSeason(AUTUMN);
     }
 
     public void Summer()
     {
-  
+        weatherText.text = "SUMMER";
+
+        ChangeSeason(SUMMER);
+    }
+
+
+    private void ChangeSeason(int season)
+    {
+        // disable to the snow/rain effects
+        globalMaterial.SetFloat("_SnowFade", 0);
+
+        snowFX.gameObject.SetActive(false);
+        rainFX.gameObject.SetActive(false);
+
+        // reset the season global volume priorities
+        autumnVolume.priority = 0;
+        rainVolume.priority = 0;
+        summerVolume.priority = 0;
+        winterVolume.priority = 0;
+       
+
+        // select which season to show
+        switch (season)
+        {
+            case AUTUMN:
+
+                autumnVolume.priority = 1;
+
+                break;
+
+            case SPRING:
+
+                rainVolume.priority = 1;
+
+                rainFX.SetActive(true);
+
+                break;
+
+            case SUMMER:
+
+                summerVolume.priority = 1;
+
+                break;
+
+            case WINTER:
+
+                winterVolume.priority = 1;
+
+                globalMaterial.SetFloat("_SnowFade", 1);
+
+                snowFX.SetActive(true);
+
+                break;
+        }
     }
 
 }
